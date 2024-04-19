ac6b66b1fad9a7f88af97f116a6591b5c250437c
ryusei33 <166240161+ryusei33@users.noreply.github.com>
2024-04-19 18:31:37 +0800

    Add files via upload

---
+++ aws-live-main/config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cfoyoiawe2dr.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "lookwaiwy-employee"
+customregion = "us-east-1"
+
